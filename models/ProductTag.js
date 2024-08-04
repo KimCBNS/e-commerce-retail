5ebafebf10bca512215048fb0496eb2c08e7be08
KimCBNS <desveaux.kim@gmail.com>
2024-08-04 09:23:06 -0300

    database and tables created including relationships

---
+++ models/ProductTag.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    // define columns
+    //  product_id: 1, tag_id: 6,
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+      allowNull: false,
+      onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+      allowNull: false,
+      onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+    },
+
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,17 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    // tag_name: 'rock music',
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+
   },
   {
     sequelize,
